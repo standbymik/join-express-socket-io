fe6afbec9fe9c23a513cee9ebaa5e92c2e45ad52
mik <standbypm4ever@gmail.com>
2018-08-20 13:37:57 +0700

    first

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-
-app.listen(3000);
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
+server.listen(3000);
 
 var clients = {};
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+app.set('view engine','ejs')
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
 
-io.sockets.on('connection', function(socket){
-  socket.on('subscribe', function(room) { 
-      console.log('joining room', room);
-      socket.join(room); 
+app.get('/',(req,res)=>{
+  res.render('index')
+})
+
+io.sockets.on('connection', function (socket) {
+  socket.on('subscribe', function (room) {
+    console.log('joining room', room);
+    socket.join(room);
   })
 
-  socket.on('unsubscribe', function(room) {  
-      console.log('leaving room', room);
-      socket.leave(room); 
+  socket.on('unsubscribe', function (room) {
+    console.log('leaving room', room);
+    socket.leave(room);
   })
 
-  socket.on('send', function(data) {
-      console.log('sending message');
-      io.sockets.in(data.room).emit('message', data);
+  socket.on('send', function (data) {
+    console.log('sending message');
+    io.sockets.in(data.room).emit('message', data);
   });
 });
+
 
 
 
