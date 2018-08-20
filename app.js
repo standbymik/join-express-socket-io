d41082fb610f8cc100d8de6edf7b3f20c13837b4
mik <standbypm4ever@gmail.com>
2018-08-20 14:14:04 +0700

    edit1

--- app.js
+++ app.js
@@ -2,18 +2,52 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
+var session = require("express-session")({
+  secret: "my-secret",
+  resave: true,
+  saveUninitialized: true
+});
+var sharedsession = require("express-socket.io-session");
+
 server.listen(3000);
+
+app.use(session);
+
+// Use shared session middleware for socket.io
+// setting autoSave:true
+io.use(sharedsession(session));
 
 var clients = {};
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-
-app.get('/',(req,res)=>{
-  res.render('index')
+app.use((req,res,next)=>{
+  req.session.user = 'mik'
+  next()
 })
 
+app.get('/', (req, res) => {
+  if(!req.session.user)
+    res.send('Please login')
+  res.render('index',{user:req.session.user})
+})
+
+io.use(sharedsession(session, {
+  autoSave: true
+}));
+
 io.sockets.on('connection', function (socket) {
+  socket.on("login", function (userdata) {
+    socket.handshake.session.userdata = userdata;
+    socket.handshake.session.save();
+  });
+  socket.on("logout", function (userdata) {
+    if (socket.handshake.session.userdata) {
+      delete socket.handshake.session.userdata;
+      socket.handshake.session.save();
+    }
+  });
+
   socket.on('subscribe', function (room) {
     console.log('joining room', room);
     socket.join(room);
